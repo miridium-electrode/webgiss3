17b82162e9cee7ac980d75302ee05f60efa6d182
miridium-electrode <checkerdance@yahoo.com>
2021-12-16 06:04:05 +0700

    three curent svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,38 +1,5 @@
 <script>
-	import * as L from 'leaflet';
-	import 'leaflet-css'
-	let m;
-
-	function createMap(container) {
-		let map = L.map(container).setView([51.505, -0.09], 13);
-	
-		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(map);
-	
-		L.marker([51.5, -0.09]).addTo(map)
-			.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-			.openPopup();
-		
-			return map;
-	}
-
-	function leaflet(container) {
-		m = createMap(container);
-
-		return {
-			destroy: () => {
-				m.remove();
-			},
-		};
-	}
+	import Leaflet from "./Leaflet.svelte";
 </script>
 
-<style>
-	#map {
-		width: 100vw;
-		height: 100vh;
-	}
-</style>
-
-<div id="map" use:leaflet></div>
+<Leaflet/>
