721119ede3ef87554d1ce08ea1d31e9573cb3a57
miridium-electrode <checkerdance@yahoo.com>
2021-12-15 16:11:54 +0700

    plugin alias

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import alias from "@rollup/plugin-alias";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -42,6 +43,12 @@ export default {
 				// enable run-time checks when not in production
 				dev: !production
 			}
+		}),
+
+		alias({
+			entries: [
+				{find: 'leaflet-css', replacement: 'leaflet/dist/leaflet.css'}
+			]
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
