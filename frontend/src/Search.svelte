cb356cc2ddee0d93f60bd054f0cd91d7088c9658
miridium-electrode <checkerdance@yahoo.com>
2021-12-16 06:28:53 +0700

    search

---
+++ frontend/src/Search.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
+	import instantsearch from "instantsearch.js";
+
+	function search(node) {
+		const isearch = instantsearch({
+			indexName: 'uni',
+			searchClient: instantMeiliSearch('http://localhost:7700', 'masterk')
+		});
+
+		isearch.addWidgets([
+			instantsearch.widgets.searchBox({
+				container: node
+			})
+		]);
+	}
+</script>
+
+<div use:search></div>
